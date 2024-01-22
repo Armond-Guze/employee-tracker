d68fbf87bcb1e9c6cea23d63a9d9903aa722e003
Armond-Guze <Armond.Guze@yahoo.com>
2024-01-22 17:11:38 -0500

    created schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+-- department table
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- role table
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- employee table
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const menu
+const menu = () => {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all roles',
+            'View all departments',
+            'Add employee',
+            'Add role',
+            'Add department',
+            'Update employee role',
+            'Quit'
+          ]
+    })
+}
