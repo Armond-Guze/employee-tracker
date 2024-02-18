b7cc976859da5a26d8b1858da9682d104687c244
Armond-Guze <Armond.Guze@yahoo.com>
2024-02-17 21:32:15 -0500

    added env file, server.js changes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
-const connection = mysql.connection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'MondiBani99!!',
-    database: 'employeetracker_db'
+    password: 'DB_PASSWORD',
+    database: 'DB_NAME'
 })
 
 connection.connect((err) => {
--- server.js
+++ server.js
@@ -1,30 +1,57 @@
+// Imports
 const inquirer = require('inquirer');
-// const fs = require("fs");
-// const mysql = require('mysql2');
+const connection = require('./config/connection');
 
-const menu = () => {
-    inquirer
+// Runs main menu promt and assigns functions to choices
+const mainMenu = () => {
+  inquirer
     .prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View all employees',
-            'View all roles',
-            'View all departments',
-            'Add employee',
-            'Add role',
-            'Add department',
-            'Update employee role',
-            'Quit'
-          ]
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all employees',
+        'View all roles',
+        'View all departments',
+        'Add employee',
+        'Add role',
+        'Add department',
+        'Update employee role',
+        'Quit'
+      ]
     })
-}
-
-const questions = () => {
+  
+  // Shows user the current employees 
+  const viewEmployees = () => {
+      connection.query('SELECT * FROM employee', (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          mainMenu();
+      });
+  };
+  
+    // Shows user the current employee roles
+  const viewRoles = () => {
+      connection.query('SELECT * FROM role', (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          mainMenu();
+      });
+  };
+  
+  // Shows all departments
+  const viewDepartments = () => {
+      connection.query('SELECT * FROM department', (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          mainMenu();
+      });
+  };
+  
+  // Adds new employee to db
+  const addEmployee = () => {
     inquirer
       .prompt([
-        
         {
           name: 'first_name',
           type: 'input',
@@ -46,11 +73,85 @@ const questions = () => {
           message: 'What is the employee\'s manager id?'
         }
       ])
-    }
+      .then((answer) => {
+          connection.query('INSERT INTO employee SET ?', answer, (err) => {
+              if (err) throw err;
+              console.log('Employee added successfully!');
+              mainMenu();
+          });
+      });
+  };
+  
+  // Adds new role to db
+  const addRole = () => {
+    inquirer
+      .prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'What is the title of the role?'
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'What is the salary for the role?'
+        },
+        {
+          name: 'department_id',
+          type: 'input',
+          message: 'What is the department id for the role?'
+        }
+      ])
+      .then((answer) => {
+          connection.query('INSERT INTO role SET ?', answer, (err) => {
+            if (err) throw err;
+            console.log('Role added successfully!');
+            mainMenu();
+        });
+    });
+};
 
-    inquirer.prompt(questions).then((responses) => {
-        console.log("Generated logo.svg");
-        console.log(responses);
-    })
-menu()
-questions()
+// Adds new department to db
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: 'What is the name of the department?'
+      }
+    ])
+    .then((answer) => {
+        connection.query('INSERT INTO department SET ?', answer, (err) => {
+            if (err) throw err;
+            console.log('Department added successfully!');
+            mainMenu();
+        });
+    });
+};
+
+// Updates employee id and role id 
+const updateEmployeeRole = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'employee_id',
+        type: 'input',
+        message: 'What is the id of the employee whose role you would like to update?'
+      },
+      {
+        name: 'new_role_id',
+        type: 'input',
+        message: 'What is the new role id for this employee?'
+      }
+    ])
+    .then((answer) => {
+        connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [answer.new_role_id, answer.employee_id], (err) => {
+            if (err) throw err;
+            console.log('Employee role updated successfully!');
+            mainMenu();
+        });
+    });
+};
+
+mainMenu(); 
